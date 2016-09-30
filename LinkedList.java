ba18211a9a6be6bf4f3adfc164d38e60791efd92
Savannah Ostrowski <savannahostrowski@gmail.com>
2016-09-29 21:23:10 -0700

    partition linked list

--- LinkedList.java
+++ LinkedList.java
@@ -5,15 +5,16 @@ import java.util.HashSet;
 public class LinkedList {
     public static void main(String[] args) {
 
-        Node fourth = new Node(7, null);
-        Node third = new Node(7, fourth);
+        Node fifth = new Node(1, null);
+        Node fourth = new Node(7, fifth);
+        Node third = new Node(9, fourth);
         Node second = new Node(7, third);
         Node first = new Node(7, second);
-        Node.printLinkedList(first);
-        Node.removeDuplicates(first);
-        Node.printLinkedList(first);
-
-
+//        Node.printLinkedList(first);
+//        Node.removeDuplicates(first);
+//        Node.printLinkedList(first);
+//        Node.printNode(Node.findKth(first, 1));
+        Node.printLinkedList(Node.partition(first, 3));
 
     }
 
@@ -28,6 +29,10 @@ public class LinkedList {
         public Node(int data, Node node) {
             this.data = data;
             next = node;
+        }
+
+        public static void printNode(Node n) {
+            System.out.println(n.data);
         }
 
         public void appendToTail(int d) {
@@ -76,6 +81,62 @@ public class LinkedList {
                     n = n.next;
                 }
             }
+        }
+
+        public static Node findKth(Node n, int k) {
+            if (k > length(n)) {
+                return null;
+            }
+            int counter = 0;
+            int length = length(n);
+            while (n.next != null) {
+                int distanceFromEnd = length - counter - 1;
+                if (distanceFromEnd == k) {
+                    return n;
+                }
+                n = n.next;
+                counter++;
+            }
+            if ((length(n) - counter) == k) {
+                return n;
+            }
+
+            return null;
+        }
+
+         public static Node appendList(Node L1, Node L2) {
+             while (L1.next != null) {
+                 L1 = L1.next;
+             }
+             L1.next = L2;
+             return L1;
+         }
+
+         public static Node partition(Node n, int x) {
+
+            Node storeLarger = null;
+            Node storeSmaller = null;
+
+            while(n != null) {
+                if (n.data < x) {
+                    if (storeSmaller == null) {
+                        storeSmaller = new Node(n.data, null);
+                    } else {
+                        storeSmaller.appendToTail(n.data);
+                    }
+                } else {
+                    if (storeLarger == null) {
+                        storeLarger = new Node(n.data, null);
+                    } else {
+                        storeLarger.appendToTail(n.data);
+                    }
+                }
+                n = n.next;
+            }
+
+
+            appendList(storeSmaller, storeLarger);
+            return storeSmaller;
         }
     }
 
