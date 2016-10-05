b74c03623c3a533de3b394c1acb3732a24f7183a
Savannah Ostrowski <savannahostrowski@gmail.com>
2016-10-05 10:51:46 -0700

    return smallest and largest revision

--- BitManipulation.java
+++ BitManipulation.java
@@ -3,34 +3,40 @@ import java.util.ArrayList;
 public class BitManipulation {
     public static void main (String[] args) {
 
-        System.out.println("Binary string: " + convertToBinary(2));
+        System.out.println("Binary string: " + convertToBinary(8));
         System.out.println("Number of 1s in the string: " + countOnes(convertToBinary(8)));
-        System.out.println("return: " + returnSmallestLargest(1));
+        System.out.println("return: " + returnSmallestLargest(8));
 
     }
 
-    public static String convertToBinary(int num) {
-        ArrayList<Integer> numbers = new ArrayList<>();
-        while (num >= 1) {
-            numbers.add(num % 2);
-            num = num / 2;
-        }
-        StringBuilder builder = new StringBuilder();
-        for(Integer value: numbers){
-            builder.append(value.toString());
-        }
-        String binary = builder.reverse().toString();
-        return binary;
+    public static int log2(int value) {
+        return Integer.SIZE-Integer.numberOfLeadingZeros(value);
     }
 
-    public static int countOnes(String binary) {
-        String[] result = binary.split("");
-        int[] ints = new int[result.length];
+    public static ArrayList<Boolean> convertToBinary(int num) {
+        int temp = num;
+        ArrayList<Boolean> bits  = new ArrayList<>();
+        int numOfBits = 0;
+        while (num > 0) {
+            numOfBits++;
+            num = num >> 1;
+        }
+        num = temp;
+
+        for (int i = numOfBits - 1; i >= 0; i--) {
+            boolean b = (num & (1 << i)) != 0;
+            System.out.println("(" + num + "&(1 << " + i +"))!= 0");
+            bits.add(b);
+        }
+
+        return bits;
+    }
+
+    public static int countOnes(ArrayList<Boolean> binary) {
         int numOfOnes = 0;
-        for(int i = 0; i < result.length; i++) {
-            int digit = Integer.valueOf(result[i]);
-            ints[i] = digit;
-            if (digit == 1) {
+        for(int i = 0; i < binary.size(); i++) {
+
+            if(binary.get(i).equals(true)) {
                 numOfOnes++;
             }
         }
@@ -39,11 +45,11 @@ public class BitManipulation {
 
     public static ArrayList<Integer> returnSmallestLargest (int num) {
         ArrayList<Integer> smallestAndLargest = new ArrayList<>();
-        String binary = convertToBinary(num);
+        ArrayList<Boolean> binary = convertToBinary(num);
         int numberOfOnes = countOnes(binary);
 
         for (int j = num - 1; j > 0; j--) {
-            String binaryJ = convertToBinary(j);
+            ArrayList<Boolean> binaryJ = convertToBinary(j);
 
             int onesJ = countOnes(binaryJ);
             if (onesJ == numberOfOnes) {
@@ -53,7 +59,7 @@ public class BitManipulation {
         }
 
         for (int k = num + 1; k < Integer.MAX_VALUE; k++) {
-            String binaryK = convertToBinary(k);
+            ArrayList<Boolean> binaryK = convertToBinary(k);
             int onesK = countOnes(binaryK);
             if (onesK == numberOfOnes) {
                 smallestAndLargest.add(k);
